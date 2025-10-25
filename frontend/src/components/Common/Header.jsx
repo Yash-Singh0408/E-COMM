d4333bbddeaf08ef837c2dc641bab0f16970bf4a
Yash Singh <yashsinghofficial04@gmail.com>
2025-10-25 18:03:12 +0530

    error debugs

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import TopBar from '../Layout/TopBar'
 import Navbar from './Navbar'
 
 const Header = () => {
   return (
     <header className='border-b border-gray-200'>
-      {/* <TopBar /> */}
       <Navbar />
-      {/* Cart Drawer */}
     </header>
   )
 }
